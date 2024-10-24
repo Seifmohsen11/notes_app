7d39a5da189321d26b918abb89718e8003702a27
Seifmohsen11 <seifmohsen859@gmail.com>
2024-10-24 21:21:39 +0300

    Create Notes List View

--- lib/views/widgets/custom_search_icon.dart
+++ lib/views/widgets/custom_search_icon.dart
@@ -12,7 +12,7 @@ class CustomSearchIcon extends StatelessWidget {
         color: Colors.white.withOpacity(.05),
         borderRadius: BorderRadius.circular(16),
       ),
-      child: Center(
+      child: const Center(
         child: Icon(Icons.search),
       ),
     );
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:notes_app/views/widgets/custom_note_item.dart';
 
 import 'custom_app_bar.dart';
+import 'notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         children: [
           SizedBox(
             height: 60,
           ),
           CustomAppBar(),
-          NoteItem(),
+          Expanded(child: NotesListView())
         ],
       ),
     );
