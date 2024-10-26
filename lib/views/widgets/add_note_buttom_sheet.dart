1171cad1f8d59748cd89bcc44ff294bddc8c9596
Seifmohsen11 <seifmohsen859@gmail.com>
2024-10-26 20:38:20 +0300

    finish Notes Buttom Sheet

--- lib/views/widgets/add_note_buttom_sheet.dart
+++ lib/views/widgets/add_note_buttom_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'custom_bottom.dart';
 import 'custom_text_field.dart';
 
 class AddNoteButtomSheet extends StatelessWidget {
@@ -9,22 +10,31 @@ class AddNoteButtomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 40,
-          ),
-          CustomTextField(
-            hintText: 'Title',
-          ),
-          SizedBox(
-            height: 18,
-          ),
-          CustomTextField(
-            hintText: 'Content',
-            maxLines: 5,
-          )
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 40,
+            ),
+            CustomTextField(
+              hintText: 'Title',
+            ),
+            SizedBox(
+              height: 18,
+            ),
+            CustomTextField(
+              hintText: 'Content',
+              maxLines: 5,
+            ),
+            SizedBox(
+              height: 45,
+            ),
+            CustomBottom(),
+            SizedBox(
+              height: 45,
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/views/widgets/custom_bottom.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'costants.dart';
+
+class CustomBottom extends StatelessWidget {
+  const CustomBottom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 55,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: kPrimaryColor,
+      ),
+      child: Center(
+        child: Text(
+          'Add',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
