65ac7dc4b8be8a4d6b4685ded1088298f49de27e
Seifmohsen11 <seifmohsen859@gmail.com>
2024-10-26 20:50:31 +0300

    create Edit Note View

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,51 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: Colors.yellow, borderRadius: BorderRadius.circular(16)),
-      child: Padding(
-        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: const Text(
-                "Flutter Tips",
-                style: TextStyle(color: Colors.black, fontSize: 28),
-              ),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(
-                  top: 16,
-                  bottom: 8,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) {
+            return EditNoteView();
+          }),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.yellow, borderRadius: BorderRadius.circular(16)),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 24, bottom: 24, left: 12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: const Text(
+                  "Flutter Tips",
+                  style: TextStyle(color: Colors.black, fontSize: 28),
                 ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 16,
+                    bottom: 8,
+                  ),
+                  child: Text(
+                    "Build your careere with seif mohsen",
+                    style: TextStyle(
+                        color: Colors.black.withOpacity(.5), fontSize: 16),
+                  ),
+                ),
+                trailing: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      FontAwesomeIcons.trash,
+                      size: 26,
+                      color: Colors.black,
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 24),
                 child: Text(
-                  "Build your careere with seif mohsen",
-                  style: TextStyle(
-                      color: Colors.black.withOpacity(.5), fontSize: 16),
+                  "May 11,2024",
+                  style: TextStyle(color: Colors.black.withOpacity(.5)),
                 ),
-              ),
-              trailing: IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    FontAwesomeIcons.trash,
-                    size: 26,
-                    color: Colors.black,
-                  )),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 24),
-              child: Text(
-                "May 11,2024",
-                style: TextStyle(color: Colors.black.withOpacity(.5)),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
