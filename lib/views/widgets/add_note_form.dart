1355446499d95ca37dbaa5aa283d9ccd5c872615
Seifmohsen11 <seifmohsen859@gmail.com>
2024-10-30 15:41:09 +0300

    add intl package and formate date

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 
@@ -54,12 +55,15 @@ class _AddNoteFormState extends State<AddNoteForm> {
               return CustomBottom(
                 isLoading: state is AddNoteLoading ? true : false,
                 onTap: () {
+                  var currentDate = DateTime.now();
+                  var formatedCurrentDate =
+                      DateFormat('dd/MM/yyyy').format(currentDate);
                   if (formKey.currentState!.validate()) {
                     formKey.currentState!.save();
                     var noteModel = NoteModel(
                         title: title!,
                         subTitle: subTitle!,
-                        date: DateTime.now().toString(),
+                        date: formatedCurrentDate,
                         color: Colors.blue.value);
                     BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
                   } else {
