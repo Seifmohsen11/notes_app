762f8a0c0735fd91000c6592b58197f6ef893856
Seifmohsen11 <seifmohsen859@gmail.com>
2024-10-30 15:13:06 +0300

    display data

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
+  const NoteItem({super.key, required this.note});
+  final NoteModel note;
 
   @override
   Widget build(BuildContext context) {
@@ -18,16 +20,16 @@ class NoteItem extends StatelessWidget {
       },
       child: Container(
         decoration: BoxDecoration(
-            color: Colors.yellow, borderRadius: BorderRadius.circular(16)),
+            color: Color(note.color), borderRadius: BorderRadius.circular(16)),
         child: Padding(
           padding: const EdgeInsets.only(top: 24, bottom: 24, left: 12),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               ListTile(
-                title: const Text(
-                  "Flutter Tips",
-                  style: TextStyle(color: Colors.black, fontSize: 28),
+                title: Text(
+                  note.title,
+                  style: const TextStyle(color: Colors.black, fontSize: 28),
                 ),
                 subtitle: Padding(
                   padding: const EdgeInsets.only(
@@ -35,7 +37,7 @@ class NoteItem extends StatelessWidget {
                     bottom: 8,
                   ),
                   child: Text(
-                    "Build your careere with seif mohsen",
+                    note.subTitle,
                     style: TextStyle(
                         color: Colors.black.withOpacity(.5), fontSize: 16),
                   ),
@@ -51,7 +53,7 @@ class NoteItem extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.only(right: 24),
                 child: Text(
-                  "May 11,2024",
+                  note.date,
                   style: TextStyle(color: Colors.black.withOpacity(.5)),
                 ),
               )
